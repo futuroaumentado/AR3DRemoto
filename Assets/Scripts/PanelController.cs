57d1c7386188be3cbba99fa210024c221fb82a58
luisjimenez29 <ljimenez29.76@gmail.com>
2020-03-28 09:20:07 -0400

    Initial commit

---
+++ Assets/Scripts/PanelController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class PanelController : MonoBehaviour
+{
+
+    public RectTransform panel;
+
+    public void AbrirPanel()
+    {
+        panel.DOAnchorPos(new Vector2(0, 0), 0.5f);
+    }
+
+
+    public void OcultarPanel()
+    {
+        panel.DOAnchorPos(new Vector2(812, 0), 0.5f);
+    }
+}
