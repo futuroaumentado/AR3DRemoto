c873a963531c938f64c8332c1f8d511ac02df626
luisjimenez29 <ljimenez29.76@gmail.com>
2020-03-29 12:27:21 -0400

    Update TargetController.cs

--- Assets/EasyAR/Scripts/TargetController.cs
+++ Assets/EasyAR/Scripts/TargetController.cs
@@ -8,9 +8,7 @@
 
 using System;
 using UnityEngine;
-using UnityEngine.Events;
-using PolyToolkit;
-using System.Collections;
+
 
 namespace easyar
 {
@@ -28,8 +26,8 @@ namespace easyar
 
         private bool firstFound;
 
-        public UnityEvent TargetFound;
-        public UnityEvent TargetLost;
+        public event Action TargetFound;
+        public event Action TargetLost;
 
         public enum ActiveControlStrategy
         {
@@ -44,39 +42,10 @@ namespace easyar
         {
             if (!IsTracked && (ActiveControl == ActiveControlStrategy.HideWhenNotTracking || ActiveControl == ActiveControlStrategy.HideBeforeFirstFound))
             {
-                ActivarRenderizador(false);
-   
+                gameObject.SetActive(false);
+
             }
         }
-
-
-        public void ObtenerAsset(string assetUrl)
-        {
-           
-             switch (primeraSeleccion)
-             {
-                 case 0:
-
-                     urlAsset = assetUrl;
-                     primeraSeleccion = 1;
-                     break;
-
-                 case 1:
-
-                    modeloPoly = GameObject.FindGameObjectsWithTag("Player");
-                    for (int i = 0; i < modeloPoly.Length; i++)
-                    {
-                        Destroy(modeloPoly[i].gameObject);
-                    }
-
-                    urlAsset = assetUrl;
-                    IsTracked = false;
-
-                    break;
-             }
-
-        }
-
 
         internal void OnTracking(bool status)
         {
@@ -87,9 +56,7 @@ namespace easyar
                    
                     if (ActiveControl == ActiveControlStrategy.HideWhenNotTracking || (ActiveControl == ActiveControlStrategy.HideBeforeFirstFound && !firstFound))
                     {
-                        ActivarRenderizador(true);
-                        //PolyApi.GetAsset(urlAsset, MyCallback);
-                        StartCoroutine(getAsset());
+                        gameObject.SetActive(true);
                     }
                     firstFound = true;
                     if (TargetFound != null)
@@ -101,13 +68,7 @@ namespace easyar
                 {
                     if (ActiveControl == ActiveControlStrategy.HideWhenNotTracking)
                     {
-                        ActivarRenderizador(false);
-                     
-                       /*modeloPoly = GameObject.FindGameObjectsWithTag("Player");
-                        for (int i = 0; i < modeloPoly.Length; i++)
-                        {
-                            Destroy(modeloPoly[i].gameObject);
-                        }*/
+                        gameObject.SetActive(false);
                     }
                     if (TargetLost != null)
                     {
@@ -123,80 +84,6 @@ namespace easyar
         }
 
         protected abstract void OnTracking();
-
-
-        IEnumerator getAsset()
-        {
-            yield return new WaitForSeconds(0.5f);
-            PolyApi.GetAsset(urlAsset, MyCallback);
-        }
-
-        void MyCallback(PolyStatusOr<PolyAsset> result)
-        {
-            if (!result.Ok)
-            {
-                // Handle error.
-                Debug.LogError("Error al obtener el asset. Razon: " + result.Status);
-                return;
-            }
-            // Success. result.Value is a PolyAsset
-            // Do something with the asset here.
-
-            PolyImportOptions options = PolyImportOptions.Default();
-
-            options.rescalingMode = PolyImportOptions.RescalingMode.FIT;
-
-            options.desiredSize = 1.0f;
-
-            options.recenter = true;
-
-
-            Debug.Log("Asset obtenido exitosamente: " + result.Value);
-
-            PolyApi.Import(result.Value, options, ImportarAsset);
-
-        }
-
-        void ImportarAsset(PolyAsset asset, PolyStatusOr<PolyImportResult> result)
-        {
-          
-
-           modeloPoly = GameObject.FindGameObjectsWithTag("Player");
-            for(int i = 0; i < modeloPoly.Length; i++)
-            {
-                Destroy(modeloPoly[i].gameObject);
-            }
-
-
-
-            if (!result.Ok)
-            {
-                // Handle error.
-                Debug.LogError("Error al importar el asset. Razon: " + result.Status);
-                return;
-            }
-            // Success. Place the result.Value.gameObject in your scene.
-            Debug.Log("Asset importado exitosamente");
-
-            marcador = GameObject.Find("ImageTarget");
-            result.Value.gameObject.transform.SetParent(marcador.transform, false);
-            //result.Value.gameObject.transform.parent = marcador.transform;
-            result.Value.gameObject.transform.position = new Vector3(0, 0, 0);
-            result.Value.gameObject.transform.Rotate(-90.0f, 0.0f, 0.0f, Space.Self);
-            result.Value.gameObject.tag = "Player";
-            urlAsset = "";
-
-
-        }
-
-        void ActivarRenderizador(bool active)
-        {
-            foreach(Renderer rend in GetComponentsInChildren<Renderer>(true))
-            {
-                rend.enabled = active;
-            }
-        }
-
         
     }
 }
